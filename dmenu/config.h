6f32a3105a91ac8b5d416dc20fc5a1fe48bec9de
Harinold <80401092+h-h-v@users.noreply.github.com>
2023-12-02 15:50:13 +0530

    Add files via upload

--- dmenu/config.h
+++ dmenu/config.h
@@ -3,6 +3,8 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
@@ -21,3 +23,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
